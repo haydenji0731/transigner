6d91525daab8f1ae134d21411d5cf0a0f6c1a279
haydenji0731 <hji20@jhu.edu>
2023-04-23 20:29:37 -0400

    add scripts for computing statistics

---
+++ scripts/calc_precision.py
@@ -0,0 +1,71 @@
+# imports
+import sys
+
+gtf_d = dict()
+# tn = unaligned
+# fn = aligned but unmapped
+
+
+def load_gtf(fname):
+    global gtf_d
+    fh = open(fname, 'r')
+    novel = 0
+    for line in fh:
+        if line[0] == "#":
+            continue
+        if line.split("\t")[2] == "transcript":
+            infos = line.split("\t")[8].split(";")
+            for info in infos:
+                key = info.strip().split(" ")[0].strip()
+                val = info.strip().split(" ")[1].strip().replace('"', '')
+                if key == "transcript_id":
+                    tid = val
+                elif key == "reference_id":
+                    rid = val.split(".")[0]
+                    break
+                elif key == "cov":
+                    rid = None
+                    novel += 1
+                    break
+        gtf_d[tid] = rid
+
+
+def load_assignment(fname):
+    fh = open(fname, 'r')
+    first = True
+    tp = 0
+    fp = 0
+    tp_conf = 0
+    fp_conf = 0
+    global gtf_d
+    for line in fh:
+        if first:
+            first = False
+            continue
+        tmp = line.split("\t")[0].split("_")
+        pred = tmp[0] + tmp[1]
+        tid = line.split("\t")[1]
+        label = gtf_d[tid]
+        conf = line.split("\t")[2]
+        if label is None or pred != label:
+            fp += 1
+            fp_conf += conf
+        elif pred == label:
+            tp += 1
+            tp_conf += conf
+    # take average
+    fp_conf /= fp
+    tp_conf /= tp
+    return tp, tp_conf, fp, fp_conf
+
+
+def main(gtf_fn, assignment_fn):
+    load_gtf(gtf_fn)
+    tp, tp_conf, fp, fp_conf = load_assignment(assignment_fn)
+    print("# of True Positives: " + str(tp))
+
+    print("# of False Positives: " + str(fp))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
---
+++ scripts/compute_loss.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+import sys
+
+def main():
+    return None
+
+
+if __name__ == "__main__":
+    main()
