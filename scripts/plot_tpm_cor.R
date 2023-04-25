1006be7ed064680a9163c4f0276e4e6f642a9a33
haydenji0731 <hji20@jhu.edu>
2023-04-25 14:31:38 -0400

    add plot R script

---
+++ scripts/plot_tpm_cor.R
@@ -0,0 +1,32 @@
+# install.packages("tidyverse")
+
+library(tidyverse)
+library(ggplot2)
+
+data_path <- "/Users/haydenji/Desktop/Classes/ela/results"
+it <- "300it"
+gt_path <- file.path(data_path, "na12878_dRNA_transcriptome_quantification.tsv")
+#pred_path <- file.path(data_path, it, "na12878_sm_both.abundance.modified.tsv")
+pred_path <- file.path(data_path, "na12878_sm_both_abundance.tsv")
+gt_quant <- read_tsv(gt_path)
+gt_quant <- gt_quant %>% rename(tpm = TPM) %>% rename(target_id = ID) %>% 
+  select(target_id, tpm) %>% filter(tpm > 0) %>% mutate(tpm = tpm + 1e-5) %>% 
+  mutate(tpm = log2(tpm))
+head(gt_quant) 
+pred_quant <- read_tsv(pred_path)
+head(pred_quant)
+
+pred_quant <- pred_quant %>% rename(target_id = transcript_id) %>% 
+  filter(target_id != "None") %>% filter(tpm > 0) %>%
+  mutate(tpm = tpm + 1e-5) %>% mutate(tpm = log2(tpm))
+head(pred_quant)
+quant_df <- inner_join(pred_quant, gt_quant, by = "target_id")
+
+quant_df <- quant_df %>% rename(predicted= tpm.x) %>% rename(actual = tpm.y)
+
+head(quant_df)
+
+g <- ggplot(quant_df, aes(x=actual, y=predicted)) + geom_point(alpha=0.05) + 
+  ggtitle("StringTie vs. Ground Truth")
+plot(g)
+
